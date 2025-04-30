eca051e48921ba606c15c5b91fe142adbb236b26
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-04-30 15:12:37 +0300

    Modal back buttons example

--- mobile/app/dashboards/student/LidersScreen/_layout.tsx
+++ mobile/app/dashboards/student/LidersScreen/_layout.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Stack } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
 import { DrawerActions } from '@react-navigation/native';
 import { menuStyles, screenStyles } from '../../../shared/styles/appStyles';
 
 
 export default function StackLayout() {
   const navigation = useNavigation();
+  const router = useRouter();
 
   return (
     <Stack>
@@ -26,9 +28,14 @@ export default function StackLayout() {
           headerShadowVisible: false,
           headerLeft: () => {
             return (
-              <Ionicons name="menu" size={24} color='#D1FF4D' style={{ marginLeft: 12 }}
-                onPress={() => { navigation.dispatch(DrawerActions.openDrawer()) }}
-              />
+              <>
+                <Ionicons name="menu" size={24} color='#D1FF4D' style={{ marginLeft: 16 }}
+                  onPress={() => { navigation.dispatch(DrawerActions.openDrawer()) }}
+                />
+                <Ionicons name='chevron-back' size={20} color='#D1FF4D' style={{ marginLeft: 22 }}
+                  onPress={() => router.back()}
+                />
+              </>
             );
           },
         }}
