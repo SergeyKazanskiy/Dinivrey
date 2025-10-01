77b600559790f469a991e5a1e95dc6410455a4eb
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-01 21:36:19 +0300

    Nginx  proxy_pass add admin_api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
-//export const API_BASE_URL = 'http://localhost:8000';
 
 export const api = axios.create({ baseURL: API_BASE_URL + `/admin_api/`});
 
@@ -11,4 +10,4 @@ export function setAuthToken(token: string) {
 }
 
 
-
+//export const API_BASE_URL = 'http://localhost:8000';
