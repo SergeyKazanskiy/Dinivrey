3f2a5848f56a740bf3b6e046fd4a864ffc29d47b
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-01 22:31:56 +0300

    Remove prefix admin_

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,10 +41,10 @@ async def setup_database():
 app.include_router(router)
 app.include_router(auth_router)
 
-app.include_router(admin_select, prefix="/admin_api") #, dependencies=[Depends(get_decoded_token)]
-app.include_router(admin_create, prefix="/admin_api")
-app.include_router(admin_update, prefix="/admin_api")
-app.include_router(admin_delete, prefix="/admin_api")
+app.include_router(admin_select, prefix="/api") #, dependencies=[Depends(get_decoded_token)]
+app.include_router(admin_create, prefix="/api")
+app.include_router(admin_update, prefix="/api")
+app.include_router(admin_delete, prefix="/api")
 
 app.include_router(student_select, prefix="/student_api", dependencies=[Depends(get_decoded_token)])
 app.include_router(student_update, prefix="/student_api", dependencies=[Depends(get_decoded_token)])
--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
 
-export const api = axios.create({ baseURL: API_BASE_URL + `/admin_api/`});
+export const api = axios.create({ baseURL: API_BASE_URL + `/api/`});
 
 export function setAuthToken(token: string) {
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
