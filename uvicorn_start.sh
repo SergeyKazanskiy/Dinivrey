1277f79d7122967c569cf510aa043dc9ef613a86
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-02 16:59:09 +0300

    Change path in uvicorn_start

--- uvicorn_start.sh
+++ uvicorn_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Запуск backend
-cd /app/backend
+
+cd /app/backend/app
 exec uvicorn main:app --host 0.0.0.0 --port 8000
+# exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
