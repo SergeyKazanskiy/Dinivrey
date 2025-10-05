d29e19daf316039a823aad10f7cd3ec0f14d2c15
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-05 07:24:26 +0300

    Fix auth create token, change firebase-key file, fix base url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+//export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export const api = axios.create({ baseURL: API_BASE_URL + `/`});
+const prefix = API_BASE_URL === "http://localhost:8000" ? `/api` : `/`
+
+export const api = axios.create({ baseURL: API_BASE_URL + prefix});
 
 export function setAuthToken(token: string) {
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
-//export const API_BASE_URL = 'http://localhost:8000';
+
