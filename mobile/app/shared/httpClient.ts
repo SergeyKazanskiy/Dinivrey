ad20eb32d4ff28b3ae68d25e02ee1823a56298b1
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-13 17:59:24 +0300

    Add cloudflared for Unraid

--- mobile/app/shared/httpClient.ts
+++ mobile/app/shared/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:8000';
-export const api = axios.create({ baseURL: API_BASE_URL + `/student_api/`});
+//export const API_BASE_URL = 'http://localhost:8000';
+export const api = axios.create({ baseURL: process.env.EXPO_PUBLIC_API_BASE_URL + `/student_api/`});
 
 
 export interface LoadingSlice {
