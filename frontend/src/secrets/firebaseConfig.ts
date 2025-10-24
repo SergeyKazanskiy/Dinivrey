7c2e12e549fe1bdc178174a28223219a0c2a096d
OPElement0 <gohmanoleg@gmail.com>
2025-10-24 13:26:14 +0300

    Update Firebase configuration with new credentials

--- frontend/src/secrets/firebaseConfig.ts
+++ frontend/src/secrets/firebaseConfig.ts
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCi12mBo2ZY8z3o2pQW_M_V5yqgf1i0SzI",
-  authDomain: "dinivrey-a4d49.firebaseapp.com",
-  projectId: "dinivrey-a4d49",
-  storageBucket: "dinivrey-a4d49.firebasestorage.app",
-  messagingSenderId: "735296528155",
-  appId: "1:735296528155:web:2a8af930c1a0b23184bdbe"
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyAcUxXuRPcEofPMi4DWrhbQHqTD6ze_uUA",
+  authDomain: "dinivreyapp-67f6f.firebaseapp.com",
+  projectId: "dinivreyapp-67f6f",
+  storageBucket: "dinivreyapp-67f6f.firebasestorage.app",
+  messagingSenderId: "441853604035",
+  appId: "1:441853604035:web:adf375e40020001481f276"
+};
