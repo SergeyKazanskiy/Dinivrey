0431e4df98309e22dc994580f6ab38998cea96d4
Sergey Kazanskiy <sergeykazanskiy74@gmail.com>
2025-10-27 14:08:11 +0200

    Change firebaseConfig on Unraid

--- frontend/src/secrets/firebaseConfig.ts
+++ frontend/src/secrets/firebaseConfig.ts
@@ -1,11 +1,11 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAQL45ipC90ITlACpU9u3XjpDoIqbq2P00",
-  authDomain: "dinivrey-4f22f.firebaseapp.com",
-  projectId: "dinivrey-4f22f",
-  storageBucket: "dinivrey-4f22f.firebasestorage.app",
-  messagingSenderId: "473620400081",
-  appId: "1:473620400081:web:3114bf11ed074e92f0c2f0"
-};
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyAQL45ipC90ITlACpU9u3XjpDoIqbq2P00",
+//   authDomain: "dinivrey-4f22f.firebaseapp.com",
+//   projectId: "dinivrey-4f22f",
+//   storageBucket: "dinivrey-4f22f.firebasestorage.app",
+//   messagingSenderId: "473620400081",
+//   appId: "1:473620400081:web:3114bf11ed074e92f0c2f0"
+// };
 
 
 
@@ -18,11 +18,11 @@ export const firebaseConfig = {
 //   appId: "1:735296528155:web:2a8af930c1a0b23184bdbe"
 // }
 
-// export const firebaseConfig = { 
-//   apiKey: "AIzaSyBdbvQtySbOo7ktdgKcFTOCUoShnE40hpY", 
-//   authDomain: "dinivreyauth.firebaseapp.com", 
-//   projectId: "dinivreyauth", 
-//   storageBucket: "dinivreyauth.firebasestorage.app", 
-//   messagingSenderId: "836946640807", 
-//   appId: "1:836946640807:web:3878ff5d5f096f262c0082" 
-// };
+export const firebaseConfig = { 
+  apiKey: "AIzaSyBdbvQtySbOo7ktdgKcFTOCUoShnE40hpY", 
+  authDomain: "dinivreyauth.firebaseapp.com", 
+  projectId: "dinivreyauth", 
+  storageBucket: "dinivreyauth.firebasestorage.app", 
+  messagingSenderId: "836946640807", 
+  appId: "1:836946640807:web:3878ff5d5f096f262c0082" 
+};
